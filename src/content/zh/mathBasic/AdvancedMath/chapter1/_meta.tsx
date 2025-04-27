2171bbdb4293fc6f43dba61171cb91a487f268bc
JackeyLove1 <1572727410@qq.com>
2025-04-27 08:43:13 +0800

    update: update function consistant

--- src/content/zh/mathBasic/AdvancedMath/chapter1/_meta.tsx
+++ src/content/zh/mathBasic/AdvancedMath/chapter1/_meta.tsx
@@ -3,4 +3,5 @@ import type { MetaRecord } from 'nextra'
 export default {
     section1: "第一节 函数",
     section2: "第二节 极限",
+    section3: "第三节 函数的连续性",
 } satisfies MetaRecord
---
+++ src/content/zh/mathBasic/AdvancedMath/chapter1/section3/_meta.tsx
@@ -0,0 +1,8 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+    1: "一、连续性的概念",
+    2: "二、连续函数的运算与初等函数的连续性",
+    3: "三、间断点及其分类",
+    4: "四、闭区间上连续函数的性质"
+} satisfies MetaRecord
